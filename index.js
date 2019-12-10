809e23ce915373dd6c1adc9ed247bfd17cbd9ee9
RockyKev <kev.rocky@gmail.com>
2019-12-09 22:01:59 -0800

    added buttons/sound effects

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+//load music
+document.addEventListener("DOMContentLoaded", function(){
+
+  console.log("music loaded")
+  const audio = document.querySelector("#intro");
+  audio.currentTime = 0;
+  // audio.play()
+
+});
+
+
+
+
+
 function playAudio(e) {
-  console.log("in play audio");
 
   //   const audio = document.querySelector(`audio [data-key="${e.keyCode}"]`);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
   console.log(e.keyCode);
-  console.log(audio);
-  console.log(key);
+
   //if there's no audio, return nothing
   if (!audio) return;
 
   //set it to start playing rom the beginnin
   audio.currentTime = 0;
   audio.play();
-  console.log(audio);
-  0;
+
   key.classList.add("playing");
 }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
-# Music maker app
+# RPG smashing sound game
 
-## Creates music
+
+## todo list
+[] - add monster health
+[] - add human health
+[] - add timed buttons, which decrease in time before reactivate
+[] - add floating text on attack
+[] - add clicking buttons
+[] - add music
+[] - Add fun messages every few seconds in the 'message log'.
+[] - 
